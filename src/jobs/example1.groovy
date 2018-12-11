4d1cf3d1a08d5e37d3e1ab103cd1f6a99912c8e5
BlackCompetencyBuilding <md.raghib0101@gmail.com>
2018-12-11 12:45:13 +0530

    Add files via upload

---
+++ src/jobs/example1.groovy
@@ -0,0 +1,16 @@
+
+String basePath = 'DSLexample'
+String repo = 'https://github.com/BlackCompetencyBuilding/DSLJobs.git'
+folder(basePath) {
+    description 'This example shows basic folder/job creation.'
+}
+
+job("$basePath/DSLExampleBuild") {
+    scm {
+        github repo
+    }
+    triggers {
+        scm '* * * * *'
+    }
+    
+}
