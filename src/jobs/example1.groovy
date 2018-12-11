7cdb2443bbc17ecbf015890b4a9c6801b68f4055
BlackCompetencyBuilding <md.raghib0101@gmail.com>
2018-12-11 13:08:54 +0530

    Update example1.groovy

--- src/jobs/example1.groovy
+++ src/jobs/example1.groovy
@@ -1,11 +1,11 @@
 
 String basePath = 'DSLexample'
 String repo = 'https://github.com/BlackCompetencyBuilding/DSLJobs.git'
-folder('$basePath') {
+folder('basePath') {
     description 'This example shows basic folder/job creation.'
 }
 
-job('$basePath/DSLExampleBuild') {
+job("$basePath/DSLExampleBuild") {
     scm {
         github repo
     }
